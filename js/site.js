d17a74eb4e9858d029051100a61ab50d202fcfc3
Jeff Lintel <jefflintel@gmail.com>
2020-03-12 19:02:37 -0500

    updated images/cards

--- js/site.js
+++ js/site.js
@@ -1,3 +1,3 @@
 $(document).ready(function(){
-    $('#biosite').tooltip({title: "This is one of the first sites I designed.", delay:{show: 0, hide: 1000}})
-}
+  $('[data-toggle="tooltip"]').tooltip();
+});
