53b6e17d81b60a0c0ed7341ba4bcb5d85bbfe761
Jeff Lintel <jefflintel@gmail.com>
2020-03-22 22:55:57 -0500

    week 5 update

--- js/site.js
+++ js/site.js
@@ -1,8 +1,20 @@
 $(document).ready(function(){
     $('#name').slideDown(1500, function () {$('#title').slideDown(1500)} ).delay(1000);
-      
-    $('[data-toggle="tooltip"]').tooltip();
 
-  
+    $('.card').mouseenter(function() {
+        var overlay = $(this).children('div.card-img-overlay');
+        $(overlay).fadeTo(1000, 0);
+        console.log('test');
+    });
+
+    $('.card').mouseleave(function() {
+        var overlay = $(this).children('div.card-img-overlay');
+        $(overlay).fadeTo(1000, 1).delay(500);
+    });
+
+    $('#email').scroll(function() {
+        $('#email').addClass("display-4").delay(500);
+    });
+     
 });
 
